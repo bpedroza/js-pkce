6b5970ae152a0ad69397196c36f3591dcc63da95
Bryan <bryan.pedroza@ymail.com>
2020-03-27 14:49:15 -0700

    1.0.1

--- src/PKCE.ts
+++ src/PKCE.ts
@@ -126,10 +126,7 @@ export default class PKCE {
    */
   private pkceChallengeFromVerifier(): string {
     const hashed = sha256(this.getCodeVerifier());
-    return Base64.stringify(hashed)
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
+    return Base64.stringify(hashed).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
   }
 
   /**
